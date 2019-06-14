43f287e65fc6040645baa287fe227e5df8c75473
david <00072416@uca.edu.sv>
2019-06-14 11:37:34 -0600

    Añadiendo linea extra.

--- src/ejecutable/Ejecutable.java
+++ src/ejecutable/Ejecutable.java
@@ -17,6 +17,7 @@ public class Ejecutable {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("PROBANDO");
+        System.out.println("AGREGANDO ESTA LINEA");
     }
     
 }
